0b51b9bdc10515e1e81613add0bbbde1a3ac0d3c
KarolJaskolka <jaskolka.karol@gmail.com>
2022-06-29 18:27:12 +0200

    Clear project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'tq-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'triple-quiz-angular';
-}
+export class AppComponent {}
