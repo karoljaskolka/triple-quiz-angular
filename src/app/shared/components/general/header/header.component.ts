a494f60474a915776ec067b707b09eb10f3855fe
KarolJaskolka <jaskolka.karol@gmail.com>
2022-08-08 16:31:54 +0200

    Add logout notification

--- src/app/shared/components/general/header/header.component.ts
+++ src/app/shared/components/general/header/header.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { faAngular } from '@fortawesome/free-brands-svg-icons';
-import { TokenService } from '../../../../core/services';
+import { NotificationService, TokenService } from '../../../../core/services';
 import { faRightFromBracket } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -14,10 +14,15 @@ export class HeaderComponent {
   faAngular = faAngular;
   faRightFromBracket = faRightFromBracket;
 
-  constructor(private tokenService: TokenService, private router: Router) {}
+  constructor(
+    private tokenService: TokenService,
+    private router: Router,
+    private notificationService: NotificationService
+  ) {}
 
   logout() {
     this.tokenService.clearToken();
     this.router.navigate(['/']);
+    this.notificationService.success('success.logout');
   }
 }
